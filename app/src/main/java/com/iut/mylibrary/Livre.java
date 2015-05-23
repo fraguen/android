1ef7b289e8cce38c16d4c5ef0bf1ca72d823f67e
William Decool <william.decool@gmail.com>
2015-05-23 22:44:02 +0200

    Ajout des classes MySQLiteHelper permettant de faire le lien entre la BDD et l'application (~= ORM). Ajout des modèles Auteur et Livre

---
+++ app/src/main/java/com/iut/mylibrary/Livre.java
@@ -0,0 +1,73 @@
+package com.iut.mylibrary;
+
+import java.util.ArrayList;
+
+/**
+ * Created by william on 23/05/15.
+ */
+
+public class Livre {
+
+    private int idLivre;
+    private String ISBN;
+    private String titre;
+    private ArrayList<Auteur> auteurs;
+
+    public Livre(){
+
+    }
+
+    public Livre(String ISBN, String titre, ArrayList<Auteur> auteurs ){
+        this.ISBN = ISBN;
+        this.titre = titre;
+        this.auteurs = auteurs;
+    }
+
+    public int getIdLivre() {
+        return idLivre;
+    }
+
+    public void setIdLivre(int idLivre) {
+        this.idLivre = idLivre;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public ArrayList<Auteur> getAuteurs() {
+        return auteurs;
+    }
+
+    public void setAuteurs(ArrayList<Auteur> auteurs) {
+        this.auteurs = auteurs;
+    }
+
+    public void addAuteur(Auteur auteur){
+        this.auteurs.add(auteur);
+    }
+
+    public boolean removeAuteur(Auteur auteur){
+        if(isAuteur(auteur)){
+            this.auteurs.remove(auteur);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isAuteur(Auteur auteur){
+        return this.auteurs.contains(auteur);
+    }
+}
