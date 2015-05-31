c5fd9b9207fb9354ac65eba662d5d90ece6f3b15
William Decool <william.decool@gmail.com>
2015-05-31 22:32:01 +0200

    Modification de la vue principale afin d'intégré le menu et l'appel au autres activité. Mise en place de la vue avec tri et listView

--- app/src/main/java/com/iut/mylibrary/MainActivity.java
+++ app/src/main/java/com/iut/mylibrary/MainActivity.java
@@ -1,8 +1,10 @@
 package com.iut.mylibrary;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -14,10 +16,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Création de la menu bar
+     * @param menu Le menu a afficher
+     * @return boolean
+     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        //R.menu.menu est l'id de notre menu
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -27,10 +37,31 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Intent i;
+        switch (id){
+            case R.id.action_settings:
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_addBook:
+                i = new Intent(MainActivity.this, AjouterLivreActivity.class);
+                this.startActivity(i);
+                return true;
+
+            case R.id.action_editBook:
+                return true;
+
+            case R.id.action_deleteBook:
+                return true;
+
+            case R.id.action_scanBook:
+                i = new Intent(MainActivity.this, ScannerActivity.class);
+                this.startActivity(i);
+                return true;
+
+            case R.id.action_exit:
+                MainActivity.this.finish();
+                System.exit(0);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
